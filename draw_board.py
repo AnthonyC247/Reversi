431d6523a2fb891e36d13105632949fa2a6e1a47
Anthony Carmona <ac5891358@gmail.com>
2023-05-31 22:39:42 -0700

    setting up the board

---
+++ draw_board.py
@@ -0,0 +1,33 @@
+import pygame 
+import sys
+import numpy as np
+
+#Initialize the game
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size) #To correspond with the appropriate display of the game
+pygame.display.set_caption("Reversi")
+
+#Set up the colors of the screen
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (0, 128, 0)
+red = (255, 0, 0)
+
+#Board and game constraints
+rows, cols = 8
+square_size = width // cols
+
+#create the initial game board 
+board = np.zeros((rows, cols), dtype=int)
+
+#Now to create the game board on the screen
+screen.fill(green)
+for row in range(rows):
+    for col in range(cols):
+        pygame.draw.rect(screen, black, (col * square_size, row * square_size, square_size, square_size))
+        if board[row][col] == 1:
+            pygame.draw.rect(screen, white, (col * square_size + square_size // 2, row * square_size + square_size // 2), square_size // 2 - 5)
+        elif board[row][col] == -1:
+            pygame.draw.circle(screen, red, (col * square_size + square_size // 2, row * square_size + square_size // 2), square_size // 2 - 5)
+pygame.display.update()
---
+++ imgs.py
@@ -0,0 +1,5 @@
+#Might not use for simplified version of the game 
+#But just in case
+#Below are the images for the white and black circles needed to play the game 
+white_image = pygame.image.load('white_piece.png')
+black_image = pygame.image.load('black_piece.png')
