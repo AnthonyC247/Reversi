c51d06fd73e655cba78b0afc9d7609638fb0358c
Anthony Carmona <ac5891358@gmail.com>
2023-08-29 10:16:12 -0700

    working version

--- draw_board.py
+++ draw_board.py
@@ -1,33 +1,19 @@
-import pygame 
+# Reversi
+import random
 import sys
-import numpy as np
 
-#Initialize the game
-pygame.init()
-size = width, height = 500, 500
-screen = pygame.display.set_mode(size) #To correspond with the appropriate display of the game
-pygame.display.set_caption("Reversi/Othello") #Display the title of the game
+def drawBoard(board):
+    # This function prints out the board that it was passed. Returns None.
+    HLINE = '  +---+---+---+---+---+---+---+---+'
+    VLINE = '  |   |   |   |   |   |   |   |   |'
 
-#Set up the colors of the screen
-black = (0, 0, 0)
-white = (255, 255, 255)
-green = (0, 128, 0)
-red = (255, 0, 0)
-
-#Board and game constraints
-rows, cols = 8 #An 8x8 board representation of the game for the output 
-square_size = width // cols
-
-#create the initial game board 
-board = np.zeros((rows, cols), dtype=int)
-
-#Now to create the game board on the screen
-screen.fill(green)
-for row in range(rows):
-    for col in range(cols):
-        pygame.draw.rect(screen, black, (col * square_size, row * square_size, square_size, square_size))
-        if board[row][col] == 1:
-            pygame.draw.rect(screen, white, (col * square_size + square_size // 2, row * square_size + square_size // 2), square_size // 2 - 5)
-        elif board[row][col] == -1:
-            pygame.draw.circle(screen, red, (col * square_size + square_size // 2, row * square_size + square_size // 2), square_size // 2 - 5)
-pygame.display.update()
+    print('    1   2   3   4   5   6   7   8')
+    print(HLINE)
+    for y in range(8):
+        print(VLINE)
+        print(y+1, end=' ')
+        for x in range(8):
+            print('| %s' % (board[x][y]), end=' ')
+        print('|')
+        print(VLINE)
+        print(HLINE)
--- game.py
+++ game.py
@@ -1,210 +1,278 @@
-import pygame
+# Reversi
+import random
 import sys
-import numpy as np
-import heapq
 
-class ReversiGame:
-    def __init__(self):
-        """
-        Initialize the Reversi game.
-        """
-        pygame.init()
-        self.size = self.width, self.height = 500, 500
-        self.screen = pygame.display.set_mode(self.size)
-        pygame.display.set_caption("Reversi/Othello")
+def drawBoard(board):
+    # This function prints out the board that it was passed. Returns None.
+    HLINE = '  +---+---+---+---+---+---+---+---+'
+    VLINE = '  |   |   |   |   |   |   |   |   |'
 
-        self.BLACK = (0, 0, 0)
-        self.WHITE = (255, 255, 255)
-        self.GREEN = (0, 128, 0)
-        self.BLUE = (0, 0, 255)
+    print('    1   2   3   4   5   6   7   8')
+    print(HLINE)
+    for y in range(8):
+        print(VLINE)
+        print(y+1, end=' ')
+        for x in range(8):
+            print('| %s' % (board[x][y]), end=' ')
+        print('|')
+        print(VLINE)
+        print(HLINE)
 
-        self.ROWS = self.COLS = 8
-        self.SQUARE_SIZE = self.width // self.COLS
-        self.board = np.zeros((self.ROWS, self.COLS))
-        self.player = 1
-        self.game_over = False
+def resetBoard(board):
+    # Blanks out the board it is passed, except for the original starting position.
+    for x in range(8):
+        for y in range(8):
+            board[x][y] = ' '
 
-    def draw_board(self):
-        """
-        Draw the game board with pieces on the screen.
-        """
-        self.screen.fill(self.GREEN)
-        for row in range(self.ROWS):
-            for col in range(self.COLS):
-                pygame.draw.rect(self.screen, self.BLACK, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE, self.SQUARE_SIZE, self.SQUARE_SIZE))
-                if self.board[row][col] == 1:
-                    pygame.draw.circle(self.screen, self.WHITE, (
-                        col * self.SQUARE_SIZE + self.SQUARE_SIZE // 2, row * self.SQUARE_SIZE + self.SQUARE_SIZE // 2), self.SQUARE_SIZE // 2 - 5)
-                elif self.board[row][col] == -1:
-                    pygame.draw.circle(self.screen, self.BLUE, (
-                        col * self.SQUARE_SIZE + self.SQUARE_SIZE // 2, row * self.SQUARE_SIZE + self.SQUARE_SIZE // 2), self.SQUARE_SIZE // 2 - 5)
-        pygame.display.update()
+    # Starting pieces:
+    board[3][3] = 'X'
+    board[3][4] = 'O'
+    board[4][3] = 'O'
+    board[4][4] = 'X'
 
-    def is_valid_move(self, row, col, player): 
-        if self.board[row][col] != 0:
-            return False
-        directions = ((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1))
-        for direction in directions:
-            if self.is_valid_direction(row, col, player, direction):
-                return True
+def getNewBoard():
+    # Creates a brand new, blank board data structure.
+    board = []
+    for i in range(8):
+        board.append([' '] * 8)
+    return board
+
+def isValidMove(board, tile, xstart, ystart):
+    # Returns False if the player's move on space xstart, ystart is invalid.
+    # If it is a valid move, returns a list of spaces that would become the player's if they made a move here.
+    if board[xstart][ystart] != ' ' or not isOnBoard(xstart, ystart):
         return False
 
-    def is_valid_direction(self, row, col, player, direction):
-        """
-        Check if a move in a specific direction is valid for the current player.
-        """
-        delta_row, delta_col = direction
-        current_row, current_col = row + delta_row, col + delta_col
-        if not (0 <= current_row < self.ROWS and 0 <= current_col < self.COLS and self.board[current_row][current_col] == -player):
-            return False
-        flips = []
-        while 0 <= current_row < self.ROWS and 0 <= current_col < self.COLS:
-            if self.board[current_row][current_col] == 0:
-                return False
-            elif self.board[current_row][current_col] == player:
-                for flip in flips:
-                    flip_row, flip_col = flip
-                    self.board[flip_row][flip_col] = player
-                return True
-            flips.append((current_row, current_col))
-            current_row += delta_row
-            current_col += delta_col
+    board[xstart][ystart] = tile  # temporarily set the tile on the board.
+
+    if tile == 'X':
+        otherTile = 'O'
+    else:
+        otherTile = 'X'
+
+    tilesToFlip = []
+
+    for xdirection, ydirection in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
+        x, y = xstart, ystart
+        x += xdirection  # first step in the direction
+        y += ydirection  # first step in the direction
+        if isOnBoard(x, y) and board[x][y] == otherTile:
+            # There is a piece belonging to the other player next to our piece.
+            x += xdirection
+            y += ydirection
+            if not isOnBoard(x, y):
+                continue
+            while board[x][y] == otherTile:
+                x += xdirection
+                y += ydirection
+                if not isOnBoard(x, y):  # break out of while loop, then continue in for loop
+                    break
+            if not isOnBoard(x, y):
+                continue
+            if board[x][y] == tile:
+                # There are pieces to flip over. Go in the reverse direction until we reach the original space, noting all the tiles along the way.
+                while True:
+                    x -= xdirection
+                    y -= ydirection
+                    if x == xstart and y == ystart:
+                        break
+                    tilesToFlip.append([x, y])
+
+    board[xstart][ystart] = ' '  # restore the empty space
+    if len(tilesToFlip) == 0:  # If no tiles were flipped, this is not a valid move.
+        return False
+    return tilesToFlip
+
+def isOnBoard(x, y):
+    # Returns True if the coordinates are located on the board.
+    return 0 <= x < 8 and 0 <= y < 8
+
+def getBoardWithValidMoves(board, tile):
+    # Returns a new board with . marking the valid moves the given player can make.
+    dupeBoard = getBoardCopy(board)
+    for x, y in getValidMoves(dupeBoard, tile):
+        dupeBoard[x][y] = '.'
+    return dupeBoard
+
+def getValidMoves(board, tile):
+    # Returns a list of [x,y] lists of valid moves for the given player on the given board.
+    validMoves = []
+    for x in range(8):
+        for y in range(8):
+            if isValidMove(board, tile, x, y) != False:
+                validMoves.append([x, y])
+    return validMoves
+
+def getScoreOfBoard(board):
+    # Determine the score by counting the tiles. Returns a dictionary with keys 'X' and 'O'.
+    xscore = 0
+    oscore = 0
+    for x in range(8):
+        for y in range(8):
+            if board[x][y] == 'X':
+                xscore += 1
+            if board[x][y] == 'O':
+                oscore += 1
+    return {'X': xscore, 'O': oscore}
+
+def enterPlayerTile():
+    # Lets the player type which tile they want to be.
+    # Returns a list with the player's tile as the first item, and the computer's tile as the second.
+    tile = ''
+    while not (tile == 'X' or tile == 'O'):
+        print('Do you want to be X or O?')
+        tile = input().upper()
+
+    # the first element in the list is the player's tile, the second is the computer's tile.
+    if tile == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
+
+def whoGoesFirst():
+    # Randomly choose the player who goes first.
+    if random.randint(0, 1) == 0:
+        return 'computer'
+    else:
+        return 'player'
+
+def playAgain():
+    # This function returns True if the player wants to play again, otherwise it returns False.
+    print('Do you want to play again? (yes or no)')
+    return input().lower().startswith('y')
+
+def makeMove(board, tile, xstart, ystart):
+    # Place the tile on the board at xstart, ystart, and flip any of the opponent's pieces.
+    # Returns False if this is an invalid move, True if it is valid.
+    tilesToFlip = isValidMove(board, tile, xstart, ystart)
+
+    if tilesToFlip == False:
         return False
 
-    def make_move(self, row, col, player):
-        """
-        Make a move for the current player and update the board accordingly.
-        """
-        self.board[row][col] = player
-        directions = ((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1))
-        for direction in directions:
-            if self.is_valid_direction(row, col, player, direction):
-                self.flip_pieces(row, col, player, direction)
+    board[xstart][ystart] = tile
+    for x, y in tilesToFlip:
+        board[x][y] = tile
+    return True
 
-    def flip_pieces(self, row, col, player, direction):
-        delta_row, delta_col = direction
-        current_row, current_col = row + delta_row, col + delta_col
-        while self.board[current_row][current_col] == -player:
-            self.board[current_row][current_col] = player
-            current_row += delta_row
-            current_col += delta_col
+def getBoardCopy(board):
+    # Make a duplicate of the board list and return the duplicate.
+    dupeBoard = getNewBoard()
 
-    def get_valid_moves(self, player):
-        """
-        Get the list of valid moves for the given player.
-        """
-        valid_moves = []
-        for row in range(self.ROWS):
-            for col in range(self.COLS):
-                if self.is_valid_move(row, col, player):
-                    valid_moves.append((row, col))
-        return valid_moves
+    for x in range(8):
+        for y in range(8):
+            dupeBoard[x][y] = board[x][y]
 
-    def evaluate_board(self):
-        """
-        Evaluate the current state (score) of the board.
-        """
-        score = np.sum(self.board)
-        return score
+    return dupeBoard
 
-    def minimax(self, depth, maximizing_player):
-        # Implementation of the minimax algorithm for AI decision-making.
-        if depth == 0:
-            return self.evaluate_board()
+def isOnCorner(x, y):
+    # Returns True if the position is in one of the four corners.
+    return (x == 0 and y == 0) or (x == 7 and y == 0) or (x == 0 and y == 7) or (x == 7 and y == 7)
 
-        valid_moves = self.get_valid_moves(1 if maximizing_player else -1)
-        if maximizing_player:
-            max_eval = float('-inf')
-            for move in valid_moves:
-                row, col = move
-                new_board = self.board.copy()
-                self.make_move(row, col, 1)
-                eval = self.minimax(depth - 1, False)
-                max_eval = max(max_eval, eval)
-            return max_eval
-        else:
-            min_eval = float('inf')
-            for move in valid_moves:
-                row, col = move
-                new_board = self.board.copy()
-                self.make_move(row, col, -1)
-                eval = self.minimax(depth - 1, True)
-                min_eval = min(min_eval, eval)
-            return min_eval
-
-    def get_best_move(self, depth, player):
-        valid_moves = self.get_valid_moves(player)
-        best_move = None
-
-        if player == 1:
-            cmp_func = lambda x, y: y[1] - x[1]  # Max-heap for player 1
-        else:
-            cmp_func = lambda x, y: x[1] - y[1]  # Min-heap for player -1
-
-        move_heap = []  # Priority queue for storing moves
-        for move in valid_moves:
-            row, col = move
-            new_board = self.board.copy()
-            self.make_move(row, col, player)
-            eval_score = self.minimax(depth - 1, False)
-            heapq.heappush(move_heap, (move, eval_score))
-
-        best_move, _ = heapq.heappop(move_heap)  # Get the best move from the priority queue
-        return best_move
-
-    def show_game_over_screen(self):
-        font = pygame.font.Font(None, 36)
-        if self.evaluate_board() > 0:
-            text = font.render("White wins!", True, self.WHITE)
-        elif self.evaluate_board() < 0:
-            text = font.render("Blue wins!", True, self.BLUE)
-        else:
-            text = font.render("It's a tie!", True, self.BLACK)
-
-        text_rect = text.get_rect(center=(self.width // 2, self.height // 2))
-        self.screen.blit(text, text_rect)
-        pygame.display.update()
-        
-    def main(self):
-        # Main game loop that handles user inputs and game logic.
-        self.draw_board()
-
-        while not self.game_over:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if self.player == 1:
-                        x, y = pygame.mouse.get_pos()
-                        col = x // self.SQUARE_SIZE
-                        row = y // self.SQUARE_SIZE
-                        if self.is_valid_move(row, col, self.player):
-                            self.make_move(row, col, self.player)
-                            self.player *= -1
-                            self.draw_board()
-
-                    if self.player == -1:
-                        depth = 3
-                        best_move = self.get_best_move(depth, self.player)
-                        if best_move is not None:
-                            row, col = best_move
-                            self.make_move(row, col, self.player)
-                            self.player *= -1
-                            self.draw_board()
-
-            valid_moves = self.get_valid_moves(self.player)
-            if len(valid_moves) == 0:
-                self.game_over = True
-                score = self.evaluate_board()
-                if score > 0:
-                    print("White wins!")
-                elif score < 0:
-                    print("Blue wins!")
-                else:
-                    print("It's a tie!")
+def getPlayerMove(board, playerTile):
+    # Let the player type in their move.
+    # Returns the move as [x, y] (or returns the strings 'hints' or 'quit')
+    DIGITS1TO8 = '1 2 3 4 5 6 7 8'.split()
+    while True:
+        print('Enter your move, or type quit to end the game, or hints to turn off/on hints.')
+        move = input().lower()
+        if move == 'quit':
+            return 'quit'
+        if move == 'hints':
+            return 'hints'
+        if len(move) == 2 and move[0] in DIGITS1TO8 and move[1] in DIGITS1TO8:
+            x = int(move[0]) - 1
+            y = int(move[1]) - 1
+            if isValidMove(board, playerTile, x, y) == False:
+                continue
+            else:
                 break
+        else:
+            print('That is not a valid move. Type the x digit (1-8), then the y digit (1-8).')
+            print('For example, 81 will be the top-right corner.')
+    return [x, y]
 
-if __name__ == "__main__":
-    game = ReversiGame()
-    game.main()
+def getComputerMove(board, computerTile):
+    # Given a board and the computer's tile, determine where to
+    # move and return that move as a [x, y] list.
+    possibleMoves = getValidMoves(board, computerTile)
+
+    # randomize the order of the possible moves
+    random.shuffle(possibleMoves)
+
+    # always go for a corner if available.
+    for x, y in possibleMoves:
+        if isOnCorner(x, y):
+            return [x, y]
+
+    # Go through all the possible moves and remember the best scoring move
+    bestScore = -1
+    for x, y in possibleMoves:
+        dupeBoard = getBoardCopy(board)
+        makeMove(dupeBoard, computerTile, x, y)
+        score = getScoreOfBoard(dupeBoard)[computerTile]
+        if score > bestScore:
+            bestMove = [x, y]
+            bestScore = score
+    return bestMove
+
+def showPoints(playerTile, computerTile):
+    # Prints out the current score.
+    scores = getScoreOfBoard(mainBoard)
+    print('You have %s points. The computer has %s points.' % (scores[playerTile], scores[computerTile]))
+
+print('Welcome to Reversi!')
+
+while True:
+    # Reset the board and game.
+    mainBoard = getNewBoard()
+    resetBoard(mainBoard)
+    playerTile, computerTile = enterPlayerTile()
+    showHints = False
+    turn = whoGoesFirst()
+    print('The ' + turn + ' will go first.')
+
+    while True:
+        if turn == 'player':
+            # Player's turn.
+            if showHints:
+                validMovesBoard = getBoardWithValidMoves(mainBoard, playerTile)
+                drawBoard(validMovesBoard)
+            else:
+                drawBoard(mainBoard)
+            showPoints(playerTile, computerTile)
+            move = getPlayerMove(mainBoard, playerTile)
+            if move == 'quit':
+                print('Thanks for playing!')
+                sys.exit()  # terminate the program
+            elif move == 'hints':
+                showHints = not showHints
+                continue
+            else:
+                makeMove(mainBoard, playerTile, move[0], move[1])
+            if not getValidMoves(mainBoard, computerTile):
+                break
+            else:
+                turn = 'computer'
+        else:
+            # Computer's turn.
+            drawBoard(mainBoard)
+            showPoints(playerTile, computerTile)
+            input("Press Enter to see the computer's move.")
+            x, y = getComputerMove(mainBoard, computerTile)
+            makeMove(mainBoard, computerTile, x, y)
+            if not getValidMoves(mainBoard, playerTile):
+                break
+            else:
+                turn = 'player'
+    # Display the final score.
+    drawBoard(mainBoard)
+    scores = getScoreOfBoard(mainBoard)
+    print('X scored %s points. O scored %s points.' % (scores['X'], scores['O']))
+    if scores[playerTile] > scores[computerTile]:
+        print('You beat the computer by %s points! Congratulations!' % (scores[playerTile] - scores[computerTile]))
+    elif scores[playerTile] < scores[computerTile]:
+        print('You lost. The computer beat you by %s points.' % (scores[computerTile] - scores[playerTile]))
+    else:
+        print('The game was a tie!')
+    if not playAgain():
+        break
